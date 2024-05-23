119e11812372d466677fa7f147b45074d33e9fc6
SteadFast <sfast0258@gmail.com>
2024-05-23 21:27:55 +0100

    Write logic for the operate function

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@ const divide = (a, b) => a / b;
 const subtract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
+
+const operate = (operator, firstNumber, secondNumber) => {
+  switch (operator) {
+    case 'add':
+      add(firstNumber, secondNumber);
+      break;
+
+    case 'divide':
+      divide(firstNumber, secondNumber);
+      break;
+
+    case 'subtract':
+      subtract(firstNumber, secondNumber);
+      break;
+
+    case 'multiply':
+      multiply(firstNumber, secondNumber);
+      break;
+  }
+};
