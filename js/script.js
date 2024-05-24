e06cb5e376d5487aee137eec52304b9e47da71f5
SteadFast <sfast0258@gmail.com>
2024-05-24 08:53:16 +0100

    Make calculator work!

--- js/script.js
+++ js/script.js
@@ -13,35 +13,67 @@ const multiply = (a, b) => a * b;
 const operate = (operator, firstNumber, secondNumber) => {
   switch (operator) {
     case 'add':
-      add(firstNumber, secondNumber);
-      break;
+      return add(firstNumber, secondNumber);
 
     case 'divide':
-      divide(firstNumber, secondNumber);
-      break;
+      return divide(firstNumber, secondNumber);
 
     case 'subtract':
-      subtract(firstNumber, secondNumber);
-      break;
+      return subtract(firstNumber, secondNumber);
 
     case 'multiply':
-      multiply(firstNumber, secondNumber);
-      break;
+      return multiply(firstNumber, secondNumber);
   }
 };
 
 // DOM
+let displayValue = '';
+
 const container = document.querySelector('.container');
 const display = document.querySelector('.display');
-
-let displayValue = '';
 
 container.addEventListener('click', function (e) {
   const target = e.target;
   const type = target.dataset.type;
 
   if (type === 'number') {
-    displayValue += target.textContent;
-    display.textContent = displayValue;
+    if (firstNumber) {
+      displayValue += target.textContent;
+      display.textContent = displayValue;
+      secondNumber = Number(displayValue);
+    } else {
+      displayValue += target.textContent;
+      display.textContent = displayValue;
+    }
   }
+
+  if (type === 'operator') {
+    if (firstNumber && operator && secondNumber) {
+      displayValue = String(operate(operator, firstNumber, secondNumber));
+      display.textContent = displayValue;
+
+      firstNumber = Number(displayValue);
+      operator = target.dataset.function;
+      displayValue = '';
+    } else if (operator) {
+      operator = target.dataset.function;
+    } else {
+      firstNumber = Number(displayValue);
+      operator = target.dataset.function;
+      displayValue = '';
+    }
+  }
+
+  if (type === 'equals') {
+    if (operator) {
+      displayValue = String(operate(operator, firstNumber, secondNumber));
+      display.textContent = displayValue;
+
+      operator = '';
+      firstNumber = 0;
+      secondNumber = 0;
+    }
+  }
+
+  console.log(firstNumber, secondNumber, operator);
 });
